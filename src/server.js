fd049fd1f3daf6b0f36571fd9a04480b921d2b5b
thangpd <thangpd@vtc.vn>
2025-03-17 17:03:18 +0700

    ngay 2

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEnging from "./config/viewEngine";
 import initWebRoute from "./route/web";
+import connectDb1 from "./config/connectDB";
 require('dotenv').config();
 let app = express();
 
@@ -9,6 +10,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 viewEnging(app);
 initWebRoute(app);
+connectDb1();
 let port = process.env.PORT || 6969;
 app.listen(port, ()=> {
     console.log("be node js: "+port);
