e554c2ae20b2c241295def8346318ae95fecb5f9
thangpd <thangpd@vtc.vn>
2025-03-18 17:14:53 +0700

    video 15

---
+++ src/seeders/seeder-user.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        firstName: "Thang",
+        lastName: "Pham",
+        password: "123456",
+        email: "admin@example.com",
+        address: "admin@example.com",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
